ab805cf961609ab6fc99d45d03b479b56fa8bc60
Victor Olsson <victor@meridium.se>
2015-03-31 11:19:45 +0200

    Prevent console from closing when debugging

--- CleanCode1/Program.cs
+++ CleanCode1/Program.cs
@@ -5,8 +5,14 @@ namespace CleanCode1 {
 	class Program {
 		static void Main(string[] args) {
 			int[] primes = GeneratePrimes.Generate(100);
+
 			for (int i = 0; i < primes.Length; i++) {
-				Console.WriteLine("Prime nummer " + i + ": " + primes[i]);
+				Console.WriteLine("Prime number with index " + i + ": " + primes[i]);
+			}
+
+			if (System.Diagnostics.Debugger.IsAttached) {
+				Console.WriteLine("Press enter to close...");
+				Console.ReadLine();
 			}
 		}
 	}
